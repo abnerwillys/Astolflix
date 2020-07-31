40885d8fc425a36abd404cc615ffa27dc7c34aed
Abner Willys <abwillys001@gmail.com>
2020-07-31 16:36:18 -0400

    Day 4 of React Immersion Alura 2020

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -76,7 +76,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://astolflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
